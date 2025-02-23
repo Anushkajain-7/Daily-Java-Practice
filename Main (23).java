59924f2713118686553f496386fd871eff9d11b5
Anushka jain <163140614+Anushkajain-7@users.noreply.github.com>
2025-02-24 00:57:28 +0530

    floyd's Triangle code

---
+++ Main (23).java
@@ -0,0 +1,23 @@
+// You are using Java
+// inverted half pyramid with numbers
+
+import java.util.*;
+
+
+class Main {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int num = 0;
+      
+       for(int i=1; i<=n; i++) {
+           for(int j=1; j<=i; j++) {
+                num += 1;
+                System.out.print(num+" ");
+                
+           }
+           
+           System.out.println();
+       }
+   }
+}
