3ca589d72340fafd44762d5c9253a9e23315cc01
Anushka jain <163140614+Anushkajain-7@users.noreply.github.com>
2025-02-24 21:44:16 +0530

    Number Pyramid pattern

---
+++ Main (28).java
@@ -0,0 +1,25 @@
+// You are using Java
+
+// Solid rhombus pattern
+
+import java.util.*;
+
+class Main {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       
+       int n = sc.nextInt();
+       int num = 1;
+       
+       for(int i =1; i<=n; i++){
+           for(int j=1; j<=(n-i); j++){
+               System.out.print(" ");
+           }
+           for(int j=1; j<=i; j++){
+               System.out.print(num+" ");
+           }
+           num++;
+           System.out.println();
+       }
+   }
+}
